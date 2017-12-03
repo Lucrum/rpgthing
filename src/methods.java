28df36b451c19969c84c816fea8d252d4642a51d
Lucrum <tohelton7076@gmail.com>
2017-12-03 13:44:27 -0500

    Added groundwork for combat menu

---
+++ src/methods.java
@@ -0,0 +1,17 @@
+public class methods {
+
+    public static String processedInput;
+
+    public static String userInput(String uInput){
+        switch(uInput){
+            case "attack":{
+            }
+            case "items":{
+            }
+            case "flee":{
+            }
+        }
+        return processedInput;
+    }
+
+}
