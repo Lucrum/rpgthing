a520adf99310c57851ab7b06a6805491d79d1cb8
Jiajie Mai <33970456+Jiajie-Mai@users.noreply.github.com>
2018-01-03 23:29:24 -0500

    Update protagonist.java

--- src/character/protagonist.java
+++ src/character/protagonist.java
@@ -6,6 +6,10 @@ public class protagonist extends character {
 
 
     public item[] inventory;
+    public object[][] dataForInventory;
+    //String for name
+    //boolean for consumable
+    //int for amount
     public String[] skills;
     public int coins;
 
@@ -23,6 +27,19 @@ public class protagonist extends character {
     public int getPlayerState(){
         return playerState;
     }
-
+    
+    //Note: not tested
+    //reads the inventory
+    public String read() {
+        String output = "";
+        for (int i = 0; i < inventory.length; i++) {
+            output = (String) dataForInventory[i][0];
+            if ((boolean) dataForInventory[i][1] == true) {
+                ouput = "\nAmount: " + ((int) dataForInventory[i][2]).toString();
+            }
+            ouput = "\n";
+        }
+        return output;
+    }
 
 }
