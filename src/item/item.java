5c50a20868289ce10e414a001a820a04c488dbf0
Jiajie Mai <33970456+Jiajie-Mai@users.noreply.github.com>
2018-01-03 23:13:39 -0500

    Update item.java

--- src/item/item.java
+++ src/item/item.java
@@ -3,4 +3,9 @@ package item;
 public class item {
     protected String name;
     protected String description;
+    
+    //used for printing out the list
+    public String toString() {
+        return name;
+    }
 }
